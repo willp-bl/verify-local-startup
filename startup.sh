e0896fe374f6d8001c4ee0915869dd2c04002a93
William Palmer <willp-bl@users.noreply.github.com>
2017-12-22 20:21:36 +0000

    Don't try and start non-open apps

--- startup.sh
+++ startup.sh
@@ -26,8 +26,8 @@ mkdir -p logs
 ( bin/ocsp_responder >logs/ocsp_responder.log 2>&1 & )
 
 build_service ../verify-hub
-build_service ../ida-sample-rp
-build_service ../ida-stub-idp
+#build_service ../ida-sample-rp
+#build_service ../ida-stub-idp
 build_service ../verify-matching-service-adapter
 
 start_service stub-event-sink ../verify-hub/hub/stub-event-sink configuration/hub/stub-event-sink.yml $EVENT_SINK_PORT
@@ -36,8 +36,8 @@ start_service policy ../verify-hub/hub/policy configuration/hub/policy.yml $POLI
 start_service saml-engine ../verify-hub/hub/saml-engine configuration/hub/saml-engine.yml $SAML_ENGINE_PORT
 start_service saml-proxy ../verify-hub/hub/saml-proxy configuration/hub/saml-proxy.yml $SAML_PROXY_PORT
 start_service saml-soap-proxy ../verify-hub/hub/saml-soap-proxy configuration/hub/saml-soap-proxy.yml $SAML_SOAP_PROXY_PORT
-start_service stub-idp ../ida-stub-idp configuration/stub-idp.yml $STUB_IDP_PORT
-start_service test-rp ../ida-sample-rp configuration/test-rp.yml $TEST_RP_PORT
+#start_service stub-idp ../ida-stub-idp configuration/stub-idp.yml $STUB_IDP_PORT
+#start_service test-rp ../ida-sample-rp configuration/test-rp.yml $TEST_RP_PORT
 start_service test-rp-msa ../verify-matching-service-adapter configuration/test-rp-msa.yml $TEST_RP_MSA_PORT
 
 pushd ../verify-frontend >/dev/null
