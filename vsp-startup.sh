bd96a3f30de919a80731c8fa0eadd56bb4f0440a
William Palmer <william.palmer@digital.cabinet-office.gov.uk>
2017-12-26 21:12:39 +0000

    can start the verify-service-provider

--- vsp-startup.sh
+++ vsp-startup.sh
@@ -7,10 +7,10 @@ pushd ../verify-service-provider >/dev/null
   ./gradlew clean build installDist -x test
 popd >/dev/null
 
-export SAML_SIGNING_KEY="$(base64 data/pki/sample_rp_signing_primary.pk8)"
-export SAML_PRIMARY_ENCRYPTION_KEY="$(base64 data/pki/sample_rp_encryption_primary.pk8)"
+export SAML_SIGNING_KEY="$(base64 -w0 data/pki/sample_rp_signing_primary.pk8)"
+export SAML_PRIMARY_ENCRYPTION_KEY="$(base64 -w0 data/pki/sample_rp_encryption_primary.pk8)"
 export SERVICE_ENTITY_IDS='["http://dev-rp.local/SAML2/MD"]'
-export METADATA_TRUST_STORE="$(base64 data/pki/metadata.ts)"
+export METADATA_TRUST_STORE="$(base64 -w0 data/pki/metadata.ts)"
 
 lsof -ti:$VSP_PORT | xargs kill
 
