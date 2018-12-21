f3f80a0b15d95c474432456ea575b19a632ae1a0
William Palmer <william.palmer@digital.cabinet-office.gov.uk>
2018-12-21 16:30:31 +0000

    update vsp startup script

--- vsp-startup.sh
+++ vsp-startup.sh
@@ -14,11 +14,15 @@ fi
 
 export SAML_SIGNING_KEY="$($BASE64 data/pki/sample_rp_signing_primary.pk8)"
 export SAML_PRIMARY_ENCRYPTION_KEY="$($BASE64 data/pki/sample_rp_encryption_primary.pk8)"
-export SERVICE_ENTITY_IDS='["http://vsp.dev-rp.local/SAML2/MD"]'
+export SERVICE_ENTITY_IDS='["http://localhost:50400/metadata"]'
 export METADATA_TRUST_STORE="$($BASE64 data/pki/metadata.ts)"
 export TEST_RP_MSA_PORT=3300
-export IDA_TESTRP_MSA_ENTITYID="http://vsp.dev-rp-ms.local/SAML2/MD"
- 
+export IDA_TESTRP_MSA_ENTITYID="http://localhost:3300/matching-service/SAML2/metadata"
+export SAML_PRIMARY_SIGNING_CERT="$($BASE64 data/pki/sample_rp_signing_primary.crt)"
+export SAML_PRIMARY_ENCRYPTION_CERT="$($BASE64 data/pki/sample_rp_encryption_primary.crt)"
+export RP_TRUSTSTORE_ENCODED="$($BASE64 data/pki/relying_parties.ts)"
+export RP_TRUSTSTORE_PASSWORD=marshmallow
+
 lsof -ti:$VSP_PORT | xargs kill
 
 ./../verify-service-provider/build/install/verify-service-provider/bin/verify-service-provider server ../verify-service-provider/local-running/local-config.yml > logs/verify-service-provider_console.log &
